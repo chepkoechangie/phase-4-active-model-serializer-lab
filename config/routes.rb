d1803a27939b7dce672869bf8e93c60bd0d21039
Angela <chepkoechangela514@gmail.com>
2022-11-29 23:24:15 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tags, only: [:index, :show]
-  resources :posts, only: [:index, :show]
-  resources :authors, only: [:index, :show]
+  resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/movies/:id/summary', to: 'movies#summary'
+  get '/movie_summaries', to: 'movies#summaries'
 end
